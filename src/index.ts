43c3b9ecd021c8fda5d896817a4b94983496ea3f
b1017034 <b1017034@gmail.com>
2024-02-09 00:18:37 +0900

    use express

--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,31 @@
-import { App } from "@slack/bolt"
-import 'dotenv/config'
+// import { App } from "@slack/bolt"
+// import 'dotenv/config'
+//
+// const app = new App({
+//     token: process.env.SLACK_BOT_TOKEN,
+//     signingSecret: process.env.SLACK_SIGNING_SECRET,
+//     processBeforeResponse: true
+// });
+//
+// app.message('farmas', async ({message, say}) => {
+//     await say(`Hey there <@${message.channel}>!`)
+// });
+//
+// app.command('/farmas', async ({command, ack, respond}) => {
+//     await ack();
+//
+//     await respond(`${command.text}`)
+// });
+//
+// (async () => {
+//     await app.start(process.env.PORT || 3000)
+// })();
 
-const app = new App({
-    token: process.env.SLACK_BOT_TOKEN,
-    signingSecret: process.env.SLACK_SIGNING_SECRET,
-    processBeforeResponse: true
+import express from 'express'
+
+const app = express()
+app.post("/", (req: express.Request, res: express.Response) => {
+    console.log(req)
+    res.send("ok")
 });
-
-app.message('farmas', async ({message, say}) => {
-    await say(`Hey there <@${message.channel}>!`)
-});
-
-app.command('/farmas', async ({command, ack, respond}) => {
-    await ack();
-
-    await respond(`${command.text}`)
-});
-
-(async () => {
-    await app.start(process.env.PORT || 3000)
-})();
+app.listen(3000)
